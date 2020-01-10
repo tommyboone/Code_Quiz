0d5ce774a04a228c33b4fff47adf9d8dd39844ff
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-01-10 10:20:51 -0600

    Pseudocode for what I need to do

---
+++ questions.js
@@ -0,0 +1,29 @@
+// As a coding bootcamp student
+// I want to take a timed quiz on JavaScript fundamentals that stores high scores
+// so that I can gauge my progress compared to my peers//
+
+
+// Play proceeds as follows:
+
+
+// The user arrives at the landing page and is presented with a call-to-action to "Start Quiz." Also note the navigation option to "View Highscores" and the "Time" value set at 0.
+
+
+// Clicking the "Start Quiz" button presents the user with a series of questions. The timer is initialized with a value and immediately begins countdown.
+
+
+// Score is calculated by time remaining. Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penalty (for example, 15 seconds are subtracted from time remaining).
+
+
+// When time runs out and/or all questions are answered, the user is presented with their final score and asked to enter their initials. Their final score and initials are then stored in localStorage.
+
+
+
+
+// start quiz call-to-action
+// after start quiz is clicked, a series of questions follows
+// when questions begin, a countdown timer starts
+// Incorrect answers result in time-penalty
+// when time is out, or quiz finished, user will see their final score and enter their initials
+// Store their score and Initials in localStorage.
+// Correct and Incorecct Pop-ups
