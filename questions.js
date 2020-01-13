4a8c9c709b1392bba3be0e926e697d5f69b52f3d
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-01-13 12:37:41 -0600

    create Start Quiz function

--- questions.js
+++ questions.js
@@ -1,7 +1,81 @@
 // As a coding bootcamp student
 // I want to take a timed quiz on JavaScript fundamentals that stores high scores
 // so that I can gauge my progress compared to my peers//
+    ///etc.
 
+   var controls = document.querySelector('.controls');
+   var startButton = document.getElementById("#btn-start");
+   var questionBox = document.querySelector(".container-fluid");
+   var timeSpan = document.getElementById("#time-left");
+   var questionElement = document.getElementById('#title');
+   var choicesElement = document.getElementById("#choices");
+   
+   var shuffleQuestions, currenQuestionIndex;
+ 
+
+   startButton.addEventListener('click', startQuiz);
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+
+    {
+      title: "What is the HTML tag under which one can write the JavaScript code?",
+      choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
+      answer: "<script>"
+    },
+    {
+      title: "Which of the following is the correct syntax to display a message in an alert box using JavaScript?",
+      choices: ["alertbox();", "msg();", "msgbox();","alert();"],
+      answer: "alert();"
+    }
+
+  ];
+  
+
+ function startQuiz(){
+    console.log("Quiz Started");
+    controls.classList.add('hide');
+    questionBox.classList.remove('hide');
+    nextQuestion();
+  
+};
+  
+
+  function nextQuestion(){
+    showQuestion(shuffleQuestions[currenQuestionIndex]);
+
+  }
+
+  function showQuestion(question){
+    questionElement.innerHTML = title.question;
+  }
+
+  function selectAnswer(){
+
+  }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+startQuiz()
 
 // Play proceeds as follows:
 
@@ -27,3 +101,4 @@
 // when time is out, or quiz finished, user will see their final score and enter their initials
 // Store their score and Initials in localStorage.
 // Correct and Incorecct Pop-ups
+
